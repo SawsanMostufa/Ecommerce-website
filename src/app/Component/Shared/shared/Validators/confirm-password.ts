61810f2f5739fd3954dde2ca4b777c41c72a1daf
SawsanMostufa <sawsanmostufa96@gmail.com>
2022-11-09 14:10:29 +0200

    validation from  password with confirm password, and conection between api and angular in registercomponent and convert input  value  of zipcode from number to string in function onsubmit() in registercomponennt

---
+++ src/app/Component/Shared/shared/Validators/confirm-password.ts
@@ -0,0 +1,12 @@
+// import { AbstractControl } from "@angular/forms";
+
+  
+
+//     export confirmPasswordValidator( control:AbstractControl)
+// {
+//    const password= control.get('password');
+//    const confirmpassword= control.get('confirmPassword');
+//    return password && confirmpassword && password.value != confirmpassword.value ? 
+//    { 'misMatch' : true }: null;
+// }
+
--- src/app/Component/User/user/component/account/register/register.component.ts
+++ src/app/Component/User/user/component/account/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AccountService } from 'src/app/Component/Shared/shared/Services/account.service';
+// import { ConfirmPassword } from 'src/app/Component/Shared/shared/Validators/confirm-password';
+import { AbstractControl } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -10,6 +12,7 @@ import { AccountService } from 'src/app/Component/Shared/shared/Services/account
 })
 export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
+  zipcode:any
   constructor( private router: Router,private fb:FormBuilder,
                private accountService:AccountService,) { }
 
@@ -22,6 +25,7 @@ export class RegisterComponent implements OnInit {
       displayName:[null, Validators.required],
       email:[null,[Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]],
       password: [null,[Validators.required]],
+      confirmPassword: [null,[Validators.required]],
       phoneNumber: [null,[Validators.required]],
       address: this.fb.group({
         firstName: [null,Validators.required],
@@ -29,19 +33,31 @@ export class RegisterComponent implements OnInit {
         street: [null,Validators.required],
         city: [null,Validators.required],
         state: [null,Validators.required],
-        zipCode: [null,Validators.required]
+        zipCode:[null,Validators.required]
       })
-    });
+      
+    } ,{validators:[this.confirmPasswordValidator]});
   }
-  get displayName(){
-   return this.registerForm.get(' displayName')
+ 
+  confirmPasswordValidator( control:AbstractControl)
+  {
+     const password= control.get('password');
+     const confirmpassword= control.get('confirmPassword');
+     if(password?.pristine || confirmpassword?.pristine)
+     {
+      return null;
+     }
+     else{
+     return password && confirmpassword && password.value != confirmpassword.value ? 
+     { 'misMatch' : true }: null;}
   }
-
+  
   onSubmit(){
     debugger
     console.log(this.registerForm.value);
+    this.registerForm.value.address.zipCode=this.registerForm.value.address.zipCode.toString()
     this.accountService.register(this.registerForm.value).subscribe(() => {
-      this.router.navigateByUrl('/home');
+    this.router.navigateByUrl('/home');
       alert('register is success')
     });
   }
